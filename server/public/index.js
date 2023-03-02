3faf7f5329f1aa518130666c3f19b0b906df5c75
eduairet <hola@eduairet.com>
2023-03-02 11:11:44 -0600

    Firebase config update

--- server/public/index.js
+++ server/public/index.js
@@ -1,10 +1,9 @@
-const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
 
-app.use(cors({ origin: true }));
+app.use(cors());
 app.use(express.json());
 
 const balances = {
@@ -46,5 +45,3 @@ function setInitialBalance(address) {
         balances[address] = 0;
     }
 }
-
-exports.app = functions.https.onRequest(app);
