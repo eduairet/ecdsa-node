154e9a78aa48600cbe3e3184679f86f9661717fe
eduairet <hola@eduairet.com>
2023-03-02 02:28:38 -0600

    Update index.js

--- client/src/server.js
+++ client/src/server.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const server = axios.create({
     baseURL: 'https://ecdsa-node.web.app',
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type',
+    },
 });
 
 export default server;
